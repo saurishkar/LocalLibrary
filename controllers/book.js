aedd2a22c0a8c33e67b9f1cb5325971b006fc5a9
vinsol <basicallysaurish@gmail.com>
2017-12-15 15:17:46 +0530

    added proper validations in book create

--- controllers/book.js
+++ controllers/book.js
@@ -50,7 +50,50 @@ exports.book_create_get = (req, res) => {
 };
 
 exports.book_create_post = (req, res) => {
-	res.send('Not Implemented: book Create Post');
+	req.sanitize('book_title').escape();
+	req.sanitize('book_title').trim();
+
+	req.sanitize('book_summary').escape();
+	req.sanitize('book_summary').trim();
+
+	req.sanitize('book_isbn').escape();
+	req.sanitize('book_isbn').trim();
+
+	req.checkBody('book_title', 'Title is Required').notEmpty();
+	req.checkBody('book_summary', 'Summary is Required').notEmpty();
+	req.checkBody('book_author', 'Author is Required').custom((value) => {
+		if(value.match(/Select an Author/) || value === '') {
+			return false;
+		}
+		return true;
+	});
+	req.checkBody('book_genre', 'Genre is Required').custom((value) => {
+		if(value.match(/Select a Genre/) || value === '') {
+			return false;
+		}
+		return true;
+	});
+
+	req.checkBody('book_isbn', 'Invalid ISBN').isISBN();
+	var data = req.body;
+	Genre.find({}).then((genres) => {
+		data.genres = genres;
+		Author.find({}).then((authors) => {
+			data.authors = authors;
+			var errors = req.validationErrors();
+
+			if(errors) {
+				console.log('errors', errors);
+				console.log('data entered', data);
+				return res.render('book/book_create', {data, errors});
+			}
+			res.send('Successfully submitted book details');
+		}, (e) => {
+			res.render('book/book_create', {error: e});
+		}, (e) => {
+			res.render('book/book_create', {error: e});
+		});
+	});
 };
 
 exports.book_delete_get = (req, res) => {
