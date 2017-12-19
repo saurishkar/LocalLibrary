cbbbfbfe403d8941c2fdc5e3e92cfa06c1381eed
vinsol <basicallysaurish@gmail.com>
2017-12-19 11:31:27 +0530

    installed compression for compressing route responses

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var config = require('dotenv').config();
+var compression = require('compression');
 var expressValidator = require('express-validator');
 
 var { mongoose } = require('./db/mongoose');
@@ -22,6 +23,8 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//
+app.use(compression()); // compress all routes
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
